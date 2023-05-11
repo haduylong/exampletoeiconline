e6961fcecb9bcdfde45858c4214edaa8e55fe7d8
haduylong <longha1032002@gmail.com>
2023-05-11 21:05:09 +0700

    commit

--- toeic-core-service-impl/src/main/java/vn/myclass/core/serviceimpl/ExaminationQuestionServiceImpl.java
+++ toeic-core-service-impl/src/main/java/vn/myclass/core/serviceimpl/ExaminationQuestionServiceImpl.java
@@ -1,6 +1,41 @@
 package vn.myclass.core.serviceimpl;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import vn.myclass.core.dto.ExaminationQuestionDTO;
+import vn.myclass.core.persistence.entity.ExaminationQuestionEntity;
 import vn.myclass.core.service.ExaminationQuestionService;
+import vn.myclass.core.serviceutils.SingletonDaoUtil;
+import vn.myclass.core.utils.ExaminationBeanUtil;
+import vn.myclass.core.utils.ExaminationQuestionBeanUtil;
 
 public class ExaminationQuestionServiceImpl implements ExaminationQuestionService{
+
+	@Override
+	public Object[] findExaminationQuestionByProperties(Map<String, Object> properties, String sortExpression,
+			String sortDirection,  Integer firstItem,  Integer maxPageItems, Integer examinationId) {
+		List<ExaminationQuestionDTO> results = new ArrayList<>();
+		String whereClause = null;
+		if(examinationId != null) {
+			whereClause = " AND examination.examinationId = " + examinationId;
+		}
+		
+		Object[] objects = SingletonDaoUtil.getExaminationQuestionDaoImplInstance().findByProperty(properties, 
+					sortExpression, sortDirection, firstItem, maxPageItems, whereClause);
+		int count = 1;
+		for(ExaminationQuestionEntity item : (List<ExaminationQuestionEntity>) objects[1]) {
+			ExaminationQuestionDTO dto = ExaminationQuestionBeanUtil.entity2Dto(item);
+			if(dto.getParagraph() == null) {// ko danh so cho doan van, chi danh so cho cau hoi
+				dto.setNumber(count);
+				count++;
+			}
+			dto.setExamination(ExaminationBeanUtil.entity2Dto(item.getExamination()));
+			results.add(dto);
+		}
+		
+		objects[1] = results;
+		return objects;
+	}
 
 }
--- toeic-web-logic/src/main/java/vn/myclass/controller/web/ExaminationQuestionController.java
+++ toeic-web-logic/src/main/java/vn/myclass/controller/web/ExaminationQuestionController.java
@@ -1,6 +1,8 @@
 package vn.myclass.controller.web;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import vn.myclass.command.ExaminationQuestionCommand;
+import vn.myclass.core.dto.ExaminationQuestionDTO;
+import vn.myclass.core.web.common.WebConstant;
+import vn.myclass.core.web.utils.FormUtil;
+import vn.myclass.core.web.utils.SingletonServiceUtil;
+
 @WebServlet(urlPatterns = {"/bai-thi-thuc-hanh.html"})
 public class ExaminationQuestionController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		ExaminationQuestionCommand command = FormUtil.populate(ExaminationQuestionCommand.class, req);
+		getListExaminationQuestion(command);
+		req.setAttribute(WebConstant.LIST_ITEMS, command);
+		req.getRequestDispatcher("/views/web/examination/detail.jsp").forward(req, resp);
 	}
 	
+	private void getListExaminationQuestion(ExaminationQuestionCommand command) {
+		Object[] objects = SingletonServiceUtil.getExaminationQuestionServiceImplInstance().findExaminationQuestionByProperties(new HashMap<String, Object>(), command.getSortExpression(), command.getSortDirection(), null, null, command.getExaminationId());
+		command.setListResult((List<ExaminationQuestionDTO>) objects[1]);
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
